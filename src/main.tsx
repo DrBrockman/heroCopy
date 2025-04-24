0c139615798c9e7a602c4ce8cf719314875b20d6
DrBrockman <derek.brockman854@topper.wku.edu>
2025-04-24 12:26:02 -0500

    router Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
+import { Provider } from "./provider.tsx"; // Import the Provider
 
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/heroCopy/">
-     
+    <HashRouter>
+      <Provider> {/* Wrap App with Provider */}
         <App />
-      
-    </BrowserRouter>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
 );
