bba939027547339725348c8900af7c9ef05bac55
DrBrockman <derek.brockman854@topper.wku.edu>
2025-04-27 16:27:53 -0500

    provider change

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
-import { PatientProvider } from "./PatientContext";
-
+import { Provider } from "./provider.tsx"; // Import the Provider
 
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <PatientProvider> {/* Wrap App with Provider */}
+      <Provider> {/* Wrap App with Provider */}
         <App />
-      </PatientProvider>
+      </Provider>
     </HashRouter>
   </React.StrictMode>,
 );
--- src/provider.tsx
+++ src/provider.tsx
@@ -3,7 +3,7 @@ import type { NavigateOptions } from "react-router-dom";
 import { HeroUIProvider } from "@heroui/system";
 import {ToastProvider} from "@heroui/toast";
 import { useHref, useNavigate } from "react-router-dom";
-
+import { PatientProvider } from "./PatientContext";
 declare module "@react-types/shared" {
   interface RouterConfig {
     routerOptions: NavigateOptions;
@@ -15,9 +15,11 @@ export function Provider({ children }: { children: React.ReactNode }) {
 
   return (
     <HeroUIProvider navigate={navigate} useHref={useHref}>
+      <PatientProvider>
       <ToastProvider maxVisibleToasts={1}
       />
       {children}
+      </PatientProvider>
     </HeroUIProvider>
   );
 }
